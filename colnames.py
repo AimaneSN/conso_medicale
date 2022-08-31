14cdf45a326ab30e39ced0f4d1c1c1d731d390e5
AimaneSounaine <ame.sounaine@gmail.com>
2022-08-31 11:44:26 +0200

    Create colnames.py

---
+++ colnames.py
@@ -0,0 +1,34 @@
+
+#BASE DEMOGRAPHIQUE
+
+ID_BENEFICIAIRE_A = "id_beneficiaire_a"
+ID_ADHERENT_A = "id_adherent_a "
+TYPE_ADHERENT = "type_adherent"
+TYPE_BENEFICIAIRE = "type_beneficiaire"
+DNAISSANCE = "dnaissance"
+SEXE = "sexe"
+ASSIETTE_COTISATION = "assiette_cotisation"
+DATE_FIN_DO = "date_fermeture_droits"
+NOMBRE_MOIS = "nombre_mois"
+VILLE = "ville"
+
+#BASE DES CONSOMMATIONS MEDICALES
+
+ID_BENEFICIAIRE_C = "id_beneficiaire_c"
+ID_ADHERENT_C = "id_adherent_c"
+RANG = "rang"
+DATE_DEBUT_SOIN = "debut_debut_soin"
+CODE_ACTE = "code_acte"
+LIBELLE_ACTE = "libelle_acte"
+PRIX_UNITAIRE = "prix_unitaire"
+MONTANT_REMBOURSE = "montant_rembourse"
+
+#BASE ALD/ALC
+
+ID_ADHERENT_ALD = "id_adherent_ald"
+ID_BENEFICIAIRE_ALD = "id_beneficiaire_ald"
+RANG_ALD = "rang_ald"
+CODE_ALD_ALC = "code_ald_alc"
+DATE_DEBUT_ACCORD_ALD = "date_debut_accord_ald"
+DATE_FIN_ACCORD_ALD = "date_fin_accord_ald"
+
