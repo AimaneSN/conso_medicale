022c6e97efac529df035f3e96d72dc2fd0675272
AimaneSounaine <ame.sounaine@gmail.com>
2022-11-27 23:39:21 +0100

    Update colnames.py

--- colnames.py
+++ colnames.py
@@ -1,6 +1,6 @@
-#Noms des colonnes (variables), les noms saisis ci-dessous sont les noms par défaut
+#Noms des colonnes (variables), les noms saisis ci-dessus sont les noms par défaut
 
-#Le script va remplacer les noms des variables par ceux-ci
+#Le script va remplacer les noms du fichier importé par ces noms
 
 
 #BASE DEMOGRAPHIQUE
@@ -22,11 +22,20 @@ VILLE = "ville"
 ID_BENEFICIAIRE_C = "id_beneficiaire_c"
 ID_ADHERENT_C = "id_adherent_c"
 RANG = "rang"
-DATE_DEBUT_SOIN = "debut_debut_soin"
+DATE_DEBUT_SOIN = "date_debut_soin"
+DATE_RECEPTION = "date_reception"
+DATE_LIQUIDAITON = "date_liquidation"
+DATE_PAIEMENT = "date_paiement"
 CODE_ACTE = "code_acte"
 LIBELLE_ACTE = "libelle_acte"
+SECTEUR_SOIN = "secteur de soins"
+TYPE_ACTE = "type_acte"
+COEFFICIENT = "coeff"
+QUANTITE = "quantite"
 PRIX_UNITAIRE = "prix_unitaire"
+TAUX_REMBOURSEMENT = "taux_remboursement"
 MONTANT_REMBOURSE = "montant_rembourse"
+MONTANT_ENGAGE = "montant_engage"
 
 #BASE ALD/ALC
 
@@ -36,4 +45,15 @@ RANG_ALD = "rang_ald"
 CODE_ALD_ALC = "code_ald_alc"
 DATE_DEBUT_ACCORD_ALD = "date_debut_accord_ald"
 DATE_FIN_ACCORD_ALD = "date_fin_accord_ald"
+
+#BASE MEDIC
+
+ID_ADHERENT_M = "id_adherent_m"
+ID_BENEFICIAIRE_M = "id_beneficiaire_m"
+RANG_MEDIC = "rang_m"
+QUANTITE_MEDIC = "quantite_medic"
+TAUX_REMBOURSEMENT_M = "taux_remboursement_m"
+PRIX_UNITAIRE_M = "prix_unitaire_m"
+CLASSE_THERAPEUTIQUE = "classe_therapeutique"
+TYPE_MEDICAMENT = "type_medicament"
 
